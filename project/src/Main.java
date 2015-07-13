53cd23656b8e528a626242fde0072cb2ef36ad1b
James Astbury <astburyj@wit.edu>
2015-07-13 10:50:10 -0400

    Parse function created, reads in a file

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,12 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Main {
-	
-	public static String compileFile = "./example_code.txt";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        new Main(args[0]);
+	}
+	
+	public Main(String fileName) {
+		try {
+            FileReader fileReader = new FileReader(fileName);
 
-		
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            parse(bufferedReader);
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
 	}
 
+	public void parse(BufferedReader br) throws IOException {
+	    String line = null;
+		while((line = br.readLine()) != null) {
+	        System.out.println(line);
+	    }
+	    br.close();
+	}
 }
+
+
+
