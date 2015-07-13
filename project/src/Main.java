0566e2443ab05c845d84f25e7b9b776295322030
mmichaud <mmichaud@millennialmedia.com>
2015-07-13 10:20:59 -0400

    eclipse project, example code

---
+++ project/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static String compileFile = "./example_code.txt";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
